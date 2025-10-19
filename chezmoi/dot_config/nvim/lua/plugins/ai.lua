dbe9b0aa3ef770c732eef5922ff2fec536aa7ddd
Sean Kay <f.sean.kay@gmail.com>
2025-10-19 10:06:37 -0400

    Remove unneeded copilot.lua configuration

--- chezmoi/dot_config/nvim/lua/plugins/ai.lua
+++ chezmoi/dot_config/nvim/lua/plugins/ai.lua
@@ -1,17 +1,4 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		opts = {
-			suggestion = { enabled = false },
-			panel = { enabled = false },
-			filetypes = {
-				markdown = true,
-				help = true,
-			},
-		},
-	},
 	-- make blink.cmp work with copilot.lua
 	{ "fang2hou/blink-copilot", opts = {} },
 	-- AI assistant
