c54d1deba286676da58f8d873a805d48dc278c36
Sean Kay <sean@snkay.com>
2025-12-28 08:13:38 -0500

    Remove mini.surround

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -3,42 +3,6 @@ return {
 	{ "nvim-mini/mini.comment", version = "*", opts = {} },
 	{ "nvim-mini/mini.pairs", version = "*", opts = {} },
 	{
-		"nvim-mini/mini.surround",
-		version = "*",
-		opts = {
-			-- Remap core actions to vim-surround keys
-			mappings = {
-				add = "ys", -- add surroundings:  ys{motion}{char}
-				delete = "ds", -- delete surroundings: ds{char}
-				replace = "cs", -- change surroundings: cs{old}{new}
-
-				-- Disable extras you don't use / to avoid conflicts
-				find = "",
-				find_left = "",
-				highlight = "",
-				update_n_lines = "",
-				suffix_last = "l",
-				suffix_next = "n",
-			},
-		},
-		config = function(_, opts)
-			require("mini.surround").setup(opts)
-
-			-- yss : surround current line (like vim-surround)
-			-- Select the line then reuse visual 'S' from mini.surround
-			vim.keymap.set("n", "yss", function()
-				-- From first nonblank to end-of-line (minus trailing newline)
-				vim.cmd.normal({ args = { "0v$h" }, bang = true })
-				vim.cmd.normal({ args = { "S" }, bang = true })
-			end, { desc = "Surround line" })
-
-			-- Visual mode: keep 'S' to surround selection (matches vim-surround)
-			-- mini.surround already maps 'S' in Visual by default.
-			-- For 'gS' (block-style in vim-surround), just alias to S (closest behavior).
-			vim.keymap.set("x", "gS", "S", { remap = true, desc = "Surround (block-style alias)" })
-		end,
-	},
-	{
 		"saghen/blink.cmp",
 		-- optional: provides snippets for the snippet source
 		dependencies = { "rafamadriz/friendly-snippets" },
