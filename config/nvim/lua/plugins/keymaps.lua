faa33f4b6b7ca2952c72af7f36aabbaa0ad21e03
Sean Kay <sean@snkay.com>
2026-01-11 11:52:43 -0500

    use opencode

--- config/nvim/lua/plugins/keymaps.lua
+++ config/nvim/lua/plugins/keymaps.lua
@@ -99,7 +99,7 @@ return { -- which-key: discoverable keymaps for your current setup
 			{
 				"<leader>aa",
 				function()
-					require("sidekick.cli").toggle({ name = "codex", focus = true })
+					require("sidekick.cli").toggle({ name = "opencode", focus = true })
 				end,
 				desc = "Sidekick Toggle CLI",
 				mode = { "n", "v" },
