7b03d48b5a72362a3fb9529e01b4093e881d7b42
Sean Kay <sean.kay@smarterdx.com>
2026-01-16 10:44:36 -0500

    Add render-markdown

--- config/nvim/lua/plugins/util.lua
+++ config/nvim/lua/plugins/util.lua
@@ -11,4 +11,9 @@ return {
 			},
 		},
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		opts = {},
+	},
 }
