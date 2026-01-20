d7d39db972638f95b085d52cc7fb599de6cf301a
Sean Kay <sean.kay@smarterdx.com>
2026-01-20 14:41:36 -0500

    Remove no auto continue for comments

--- config/nvim/lua/config/post-lazy-opts.lua
+++ config/nvim/lua/config/post-lazy-opts.lua
@@ -99,14 +99,6 @@ vim.api.nvim_create_autocmd("VimResized", {
 	command = "wincmd =",
 })
 
--- no auto continue comments on new line
-vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
-	callback = function()
-		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-	end,
-})
-
 -- show cursorline only in active window enable
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
 	group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
