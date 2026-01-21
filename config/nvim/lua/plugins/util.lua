33670618a012660d06afe59a905fff37c042f698
Sean Kay <sean@snkay.com>
2026-01-21 08:44:39 -0500

    Add todo comments

--- config/nvim/lua/plugins/util.lua
+++ config/nvim/lua/plugins/util.lua
@@ -1,6 +1,11 @@
 return {
 	{ "christoomey/vim-tmux-navigator" },
 	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
 		"folke/lazydev.nvim",
 		ft = "lua",
 		opts = {
