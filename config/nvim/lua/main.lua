08398a7708fc0124dc504d456942d67f2b583685
Sean Kay <sean@snkay.com>
2026-01-24 22:41:37 -0500

    Center mini.pick

--- config/nvim/lua/main.lua
+++ config/nvim/lua/main.lua
@@ -33,14 +33,17 @@ require("mini.pick").setup({
     mark = '<tab>'
   },
   window = {
-    config = {
-      relative = 'cursor',
-      anchor = 'NW',
-      row = 0,
-      col = 0,
-      width = 40,
-      height = 20,
-    },
+    config = function()
+      local height = math.floor(0.618 * vim.o.lines)
+      local width = math.floor(0.618 * vim.o.columns)
+      return {
+        anchor = 'NW',
+        height = height,
+        width = width,
+        row = math.floor(0.5 * (vim.o.lines - height)),
+        col = math.floor(0.5 * (vim.o.columns - width)),
+      }
+    end,
   },
 })
 require("mini.surround").setup()
