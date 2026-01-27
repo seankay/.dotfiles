acc4ed47995df211fa05cd4ba68db85c037db80d
Sean Kay <sean@snkay.com>
2026-01-26 20:54:42 -0500

    Navs working on linux

--- config/nvim/lua/main.lua
+++ config/nvim/lua/main.lua
@@ -9,7 +9,7 @@ vim.pack.add({
   { src = "https://github.com/tpope/vim-fugitive" },
   { src = "https://github.com/tpope/vim-rhubarb" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
-  { src = "https://github.com/christoomey/vim-tmux-navigator" },
+  { src = "https://github.com/knubie/vim-kitty-navigator" },
   { src = "https://github.com/stevearc/oil.nvim" },
 })
 
