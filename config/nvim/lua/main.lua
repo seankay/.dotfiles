899e68fea7ff463258bce6b7df6028ef6f05dc42
Sean Kay <sean@snkay.com>
2026-02-07 09:47:01 -0500

    Add keybind for committing from diffview

--- config/nvim/lua/main.lua
+++ config/nvim/lua/main.lua
@@ -66,6 +66,25 @@ require('render-markdown').setup({
 })
 
 require("diffview").setup({
+  keymaps = {
+    file_panel = {
+      {
+        "n", "cc",
+        "<Cmd>Git commit <bar> wincmd J<CR>",
+        { desc = "Commit staged changes" },
+      },
+      {
+        "n", "ca",
+        "<Cmd>Git commit --amend <bar> wincmd J<CR>",
+        { desc = "Amend the last commit" },
+      },
+      {
+        "n", "c<space>",
+        ":Git commit ",
+        { desc = "Populate command line with \":Git commit \"" },
+      },
+    },
+  },
   file_panel = {
     listing_style = "list",
     win_config = {
