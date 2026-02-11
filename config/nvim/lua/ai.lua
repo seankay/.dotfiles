5b9dae06ca05c891772ecbc8b0c99047541d67d9
Sean Kay <sean.kay@smarterdx.com>
2026-02-11 17:13:24 -0500

    Revert "Use sidekick for ai"
    This reverts commit 2ff83f2bcec855c6aa9a5d4010080782032a664a.

--- config/nvim/lua/ai.lua
+++ config/nvim/lua/ai.lua
@@ -1,20 +1,16 @@
 local utils = require("utils")
 vim.pack.add({
-  { src = utils.gh("folke/sidekick.nvim") },
+  { src = utils.gh("nickvandyke/opencode.nvim") },
 })
-
-require("sidekick").setup({
-  nes = { enabled = false },
-  cli = {
-    win = {
-      keys = {
-        prompt = { "<leader>ap", "prompt", mode = "t", desc = "insert prompt or context" },
-      }
-    },
-    tools = {
-      opencode_resume = {
-        cmd = { "opencode", "--continue" }
-      }
+vim.g.opencode_opts = {
+  -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+  provider = {
+    enabled = "kitty",
+    cmd = "--copy-env --bias=30 opencode --port --continue",
+    kitty = {
+      location = "vsplit"
     }
   }
-})
+}
+-- Required for `opts.events.reload`.
+vim.o.autoread = true
