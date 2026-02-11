bdca5ba2d971fe6b2dd02a0c5af517d7ed0b9be0
Sean Kay <sean@snkay.com>
2026-02-10 19:57:49 -0500

    basic kitty ssh config

---
+++ config/kitty/ssh/.bash_profile
@@ -0,0 +1,4 @@
+export PATH=$PATH:/root/.local/bin:/root/.local/share/bob/nvim-bin/
+
+alias e=nvim
+
---
+++ config/kitty/ssh/bootstrap-server.sh
@@ -0,0 +1,6 @@
+# Bootstrap the remote server
+#
+apt update
+apt install unzip   
+curl -fsSL https://raw.githubusercontent.com/MordechaiHadad/bob/master/scripts/install.sh | bash
+
