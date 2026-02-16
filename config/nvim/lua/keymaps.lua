afda64d7167f1242470bd98f56c2e86b1745cc07
Sean Kay <sean@snkay.com>
2026-02-16 08:10:34 -0500

    Replace snacks with fzflua/mini. Remove some packages

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -17,14 +17,11 @@ require("which-key").setup({
     align = "left",
   },
   spec = {
-    { "<leader>a", group = "AI", icon = "󱚡" },
     { "<leader>g", group = "Git" },
-    { "<leader>gh", group = "Hunk" },
     { "<leader>l", group = "LSP", icon = "󰲽" },
     { "<leader>o", group = "Obsidian", icon = "" },
     { "<leader>p", group = "Pack", icon = "" },
     { "<leader>s", group = "Search", icon = "" },
-    { "<leader>t", group = "Tests", icon = "󰙨" },
     { "<leader>x", group = "Diagnostics" },
     { "<leader>e", group = "Explore", icon = "" },
     { "<leader>/", group = "Grep", icon = "" },
@@ -89,135 +86,59 @@ vim.cmd([[cabbrev Wq wq]])
 vim.cmd([[cabbrev WQ wq]])
 
 -- Git
-local gitsigns = require("gitsigns")
 map("n", "<leader>G", ":vertical G<cr>", { desc = "Git status" })
 map("n", "<leader>gd", ":vertical Gdiff<cr>", { desc = "Git Diff" })
 map("n", "<leader>gl", ":vertical G log<cr>", { desc = "Git Log" })
 map("n", "<leader>gb", ":G blame<cr>", { desc = "Git blame" })
-map('n', '<leader>ghs', gitsigns.stage_hunk, { desc = "Stage hunk" })
-map('v', '<leader>ghs', function()
-  gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-end, { desc = "Stage hunk" })
-map('n', '<leader>ghr', gitsigns.reset_hunk, { desc = "Reset hunk" })
-map('v', '<leader>ghr', function()
-  gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-end, { desc = "Reset hunk" })
-map({ 'o', 'x' }, 'gih', gitsigns.select_hunk, { desc = "Select hunk" })
-map('n', '<leader>ghp', gitsigns.preview_hunk, { desc = "Preview hunk" })
-map('n', '<leader>ghi', gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
-map('n', ']h', function()
-  if vim.wo.diff then
-    vim.cmd.normal({ ']c', bang = true })
-  else
-    gitsigns.nav_hunk('next')
+map("n", "<leader>gO", function() utils.open_github(vim.api.nvim_win_get_cursor(0)[1]) end, { desc = "Git Open" })
+map("x", "<leader>gO", function()
+  local start_line = vim.fn.line("v")
+  local end_line = vim.fn.line(".")
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
   end
-end, { desc = "Next hunk" })
-
-map('n', '[h', function()
-  if vim.wo.diff then
-    vim.cmd.normal({ '[c', bang = true })
-  else
-    gitsigns.nav_hunk('prev')
-  end
-end, { desc = "Prev hunk" })
-map({ "n", "x" }, "<leader>go", function() Snacks.gitbrowse() end, { desc = "Git browse" })
+  utils.open_github(start_line, end_line)
+end, { desc = "Git Open" })
 
 -- Pickers
-map("n", "<c-p>", function()
-  Snacks.picker.smart()
-end, { desc = "Smart picker" })
-map("n", "<leader>/", function()
-  Snacks.picker.grep()
-end, { desc = "Grep" })
-map("n", "<leader>sf", function()
-  Snacks.picker.files()
-end, { desc = "File picker" })
-map("n", "<leader>sw", function()
-  Snacks.picker.grep({ search = vim.fn.expand("<cword>") })
-end, { desc = "Grep word under cursor" })
-map("n", "<leader>b", function()
-  Snacks.picker.buffers()
-end, { desc = "Buffers" })
-map("n", "<leader>x", function()
-  Snacks.picker.diagnostics()
-end, { desc = "Diagnostics" })
-map("n", "gd", function()
-  Snacks.picker.lsp_definitions()
-end, { desc = "LSP definitions" })
-map("n", "gD", function()
-  Snacks.picker.lsp_declarations()
-end, { desc = "LSP declarations" })
-map("n", "gr", function()
-  Snacks.picker.lsp_references()
-end, { desc = "LSP references" })
-map("n", "gI", function()
-  Snacks.picker.lsp_implementations()
-end, { desc = "LSP implementations" })
-map("n", "gT", function()
-  Snacks.picker.lsp_type_definitions()
-end, { desc = "LSP type definitions" })
-map("n", "gai", function() Snacks.picker.lsp_incoming_calls() end, { desc = "C[a]lls Incoming" })
-map("n", "gao", function() Snacks.picker.lsp_outgoing_calls() end, { desc = "C[a]lls Outgoing" })
-map("n", "<leader>ls", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
-map("n", "<leader>lS", function() Snacks.picker.lsp_workspace_symbols() end, { desc = "LSP Workspace Symbols" })
-map("n", '<leader>s"', function() Snacks.picker.registers() end, { desc = "Registers" })
-map("n", '<leader>s/', function() Snacks.picker.search_history() end, { desc = "Search History" })
-map("n", "<leader>sa", function() Snacks.picker.autocmds() end, { desc = "Autocmds" })
-map("n", "<leader>sb", function() Snacks.picker.lines() end, { desc = "Buffer Lines" })
-map("n", "<leader>sc", function() Snacks.picker.command_history() end, { desc = "Command History" })
-map("n", "<leader>sC", function() Snacks.picker.commands() end, { desc = "Commands" })
-map("n", "<leader>sd", function() Snacks.picker.diagnostics() end, { desc = "Diagnostics" })
-map("n", "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, { desc = "Buffer Diagnostics" })
-map("n", "<leader>sh", function() Snacks.picker.help() end, { desc = "Help Pages" })
-map("n", "<leader>si", function() Snacks.picker.icons() end, { desc = "Icons" })
-map("n", "<leader>sj", function() Snacks.picker.jumps() end, { desc = "Jumps" })
-map("n", "<leader>sk", function() Snacks.picker.keymaps() end, { desc = "Keymaps" })
-map("n", "<leader>sl", function() Snacks.picker.loclist() end, { desc = "Location List" })
-map("n", "<leader>sm", function() Snacks.picker.marks() end, { desc = "Marks" })
-map("n", "<leader>sM", function() Snacks.picker.man() end, { desc = "Man Pages" })
-map("n", "<leader>su", function() Snacks.picker.undo() end, { desc = "Undo History" })
-
+local fzflua = require("fzf-lua")
+map("n", "<c-p>", function() fzflua.files() end)
+map("n", "<leader>/", function() fzflua.live_grep() end, { desc = "Grep" })
+map("n", "<leader>sw", function() fzflua.grep_cword() end, { desc = "Grep word under cursor" })
+map("x", "<leader>sw", function() fzflua.grep_visual() end, { desc = "Grep selected content" })
+map("n", "<leader>x", function() fzflua.diagnostics_document() end, { desc = "Document Diagnostics" })
+map("n", "<leader>X", function() fzflua.diagnostics_workspace() end, { desc = "Workspace Diagnostics" })
+map("n", "<leader>ls", function() fzflua.lsp_document_symbols() end, { desc = "LSP Symbols" })
+map("n", "<leader>lS", function() fzflua.lsp_workspace_symbols() end, { desc = "LSP Workspace Symbols" })
+map("n", "<leader>la", function() fzflua.lsp_code_actions() end, { desc = "LSP Code Actions" })
+map("n", '<leader>s"', function() fzflua.registers() end, { desc = "Registers" })
+map("n", '<leader>s/', function() fzflua.search_history() end, { desc = "Search History" })
+map("n", "<leader>sa", function() fzflua.autocmds() end, { desc = "Autocmds" })
+map("n", "<leader>sb", function() fzflua.buffers() end, { desc = "Buffer" })
+map("n", "<leader>sc", function() fzflua.command_history() end, { desc = "Command History" })
+map("n", "<leader>sC", function() fzflua.commands() end, { desc = "Commands" })
+map("n", "<leader>sh", function() fzflua.help_tags() end, { desc = "Help Pages" })
+map("n", "<leader>sj", function() fzflua.jumps() end, { desc = "Jumps" })
+map("n", "<leader>sk", function() fzflua.keymaps() end, { desc = "Keymaps" })
+map("n", "<leader>sl", function() fzflua.loclist() end, { desc = "Location List" })
+map("n", "<leader>sm", function() fzflua.marks() end, { desc = "Marks" })
+map("n", "<leader>sM", function() fzflua.manpages() end, { desc = "Man Pages" })
+map("n", "<leader>su", ":FzfLua undotree fzf_opts.--keep-right=true<cr>", { desc = "Undo History" })
+map("n", "<leader>sr", function() fzflua.resume() end, { desc = "Resume Fzf" })
+map("n", "<leader>sg", function() fzflua.global() end, { desc = "Global picker" })
+map("n", "<leader>sg", function() fzflua.tabs() end, { desc = "Tabs" })
+map("n", "<leader>sq", function() fzflua.quickfix() end, { desc = "Quickfix List" })
+-- LSP
+map("n", "gd", function() fzflua.lsp_definitions() end, { desc = "LSP definitions" })
+map("n", "gD", function() fzflua.lsp_declarations() end, { desc = "LSP declarations" })
+map("n", "gr", function() fzflua.lsp_references() end, { desc = "LSP references" })
+map("n", "gI", function() fzflua.lsp_implementations() end, { desc = "LSP implementations" })
+map("n", "gT", function() fzflua.lsp_typedefs() end, { desc = "LSP type definitions" })
+map("n", "gai", function() fzflua.lsp_incoming_calls() end, { desc = "C[a]lls Incoming" })
+map("n", "gao", function() fzflua.lsp_outgoing_calls() end, { desc = "C[a]lls Outgoing" })
 
 -- obsidian
 map("n", "<leader>oo", ":split | Obsidian new TODO<cr>", { desc = "New TODO note" })
 map("n", "<leader>of", "<cmd>:Obsidian quick_switch<cr>", { desc = "Quick switch note" })
 map("n", "<leader>og", "<cmd>:Obsidian search<cr>", { desc = "Search notes" })
 map("n", "<leader>on", "<cmd>:Obsidian new<cr>", { desc = "New note" })
-
--- AI
-map({ "n", "x" }, "<leader>aa", function() require("opencode").ask("@this: ", { submit = true }) end,
-  { desc = "Ask opencode…" })
-map({ "n", "x" }, "<leader>as", function() require("opencode").select() end, { desc = "Execute opencode action…" })
-map({ "n", "t" }, "<leader>at", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
-
-map({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
-  { desc = "Add range to opencode", expr = true })
-map("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
-  { desc = "Add line to opencode", expr = true })
-
-map("n", "<leader>au", function() require("opencode").command("session.half.page.up") end,
-  { desc = "Scroll opencode up" })
-map("n", "<leader>ad", function() require("opencode").command("session.half.page.down") end,
-  { desc = "Scroll opencode down" })
-
--- Tests (neotest)
-map("n", "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "Test file" })
-map("n", "<leader>tr", function() require("neotest").run.run() end, { desc = "Test nearest" })
-map("n", "<leader>tl", function() require("neotest").run.run_last() end, { desc = "Test last" })
-map("n", "<leader>ta",
-  function()
-    local neotest = require("neotest")
-    neotest.run.run({ suite = true })
-    neotest.summary.open()
-  end, { desc = "Test suite" })
-map("n", "<leader>tp", function() require("neotest").output_panel.toggle() end, { desc = "Toggle output panel" })
-map("n", "<leader>to", function()
-    require("neotest").output.open({
-      short = true,
-      auto_close = true,
-    })
-  end,
-  { desc = "Output window" })
-map("n", "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, { desc = "Watch file" })
-map("n", "<leader>tx", function() require("neotest").run.stop() end, { desc = "Stop" })
-map("n", "<leader>ts", function() require("neotest").summary.toggle() end, { desc = "Summary" })
